cf70f91108bd4b5fa889157c6436c7d197a0f5d5
Awejuddin kazi <123185125+awejuddinkazi@users.noreply.github.com>
2023-05-30 11:49:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import componation from "../src/cpmponent/Header";
+import Mainsection from "../src/page/mainsection";
+
+function App() {
+  return (
+    <div>
+      <Mainsection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mainsection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../cpmponent/Header'
+import Sectionone from '../cpmponent/componation'
+function Mainsection() {
+  return (
+    <div>
+        <Header/>
+        <Sectionone/>
+    </div>
+  )
+}
+
+export default Mainsection
